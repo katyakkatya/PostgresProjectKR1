f1a50a0e2561bd4bb7b76283db2b4d4b413fedbe
Denis <denisvp06@mail.ru>
2025-10-24 01:04:06 +0300

    feat: add style for the AuthorSection

--- src/main/kotlin/ui/screens/task_detail/components/AuthorSection.kt
+++ src/main/kotlin/ui/screens/task_detail/components/AuthorSection.kt
@@ -1,12 +1,19 @@
 package ui.screens.task_detail.components
 
-import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.BorderStroke
+import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.Spacer
-import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.width
+import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.material.Card
 import androidx.compose.material.MaterialTheme
 import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
 import models.UserModel
@@ -16,8 +23,10 @@ import ui.screens.common.components.UserItem
 fun AuthorSection(
   user: UserModel?,
 ) {
-  Column {
-    Spacer(modifier = Modifier.height(32.dp))
+  Row(
+    modifier = Modifier.fillMaxWidth().padding(top = 32.dp),
+    verticalAlignment = Alignment.CenterVertically
+  ){
     Text(
       text = "Автор",
       fontFamily = MaterialTheme.typography.h6.fontFamily,
@@ -26,20 +35,33 @@ fun AuthorSection(
       style = MaterialTheme.typography.h6
     )
     Spacer(
-      modifier = Modifier.height(16.dp)
+      modifier = Modifier.width(8.dp)
     )
     if (user == null) {
       Text(
-        text = "Автор не выбран",
+        text = "не выбран",
         fontFamily = MaterialTheme.typography.h6.fontFamily,
         fontSize = 24.sp,
         color = MaterialTheme.colors.onSurface,
         style = MaterialTheme.typography.h6
       )
     } else {
-      UserItem(
-        user = user
-      )
+      Spacer(modifier = Modifier.width(24.dp))
+      Card(
+        shape = RoundedCornerShape(12.dp),
+        border = BorderStroke(1.dp, MaterialTheme.colors.primary.copy(alpha = 0.2f)),
+        elevation = 2.dp,
+        backgroundColor = MaterialTheme.colors.surface
+      ){
+        Text(
+          text = user.name,
+          fontSize = 24.sp,
+          style = MaterialTheme.typography.h6,
+          color = MaterialTheme.colors.onSurface,
+          fontWeight = FontWeight.Medium,
+          modifier = Modifier.padding(vertical = 16.dp, horizontal = 32.dp)
+        )
+      }
     }
   }
 }
